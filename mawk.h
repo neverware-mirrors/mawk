26ea640cfb1025577e252087b08de088386387ec
James Troup <james@nocrew.org>
2001-07-18 20:40:37 +0100

    Import Debian changes 1.3.3-8
    mawk (1.3.3-8) unstable; urgency=low
    
      * debian/rules (build): compile with -O1 on hppa to work around probable
        compiler bug.  Thanks to LaMont Jones <lamont@ia.mmjgroup.com>.
    
    mawk (1.3.3-7) unstable; urgency=low
    
      * mawk.h: remove bogus redefinition of read() and close() and #include
        <unistd.h> instead, thanks to LaMont Jones <lamont@ia.mmjgroup.com>.
        Closes: #104124
    
    mawk (1.3.3-6) unstable; urgency=low
    
      * debian/control (Maintainer): fixed to be me. 
      * debian/changelog: remove add-log-mailing-address.
      * debian/rules: rewritten.
      * debian/control (Standards-Version): bump to 3.5.5.0.
      * Half hearted attempt at -Wall cleaning of the code.

--- mawk.h
+++ mawk.h
@@ -52,6 +52,7 @@ the GNU General Public License, version 2, 1991.
 
 #include  "nstd.h"
 #include <stdio.h>
+#include <unistd.h>
 #include "types.h"
 
 #ifdef   DEBUG
@@ -145,7 +146,7 @@ void  PROTO( DB_cell_destroy, (CELL *) ) ;
 void  PROTO( overflow, (char *, unsigned) ) ;
 void  PROTO( rt_overflow, (char *, unsigned) ) ;
 void  PROTO( rt_error, ( char *, ...) ) ;
-void  PROTO( mawk_exit, (int) ) ;
+void  PROTO( mawk_exit, (int) ) __attribute__ ((noreturn)) ;
 void PROTO( da, (INST *, FILE *)) ;
 char *PROTO( str_str, (char*, char*, unsigned) ) ;
 char *PROTO( rm_escape, (char *) ) ;
@@ -153,16 +154,13 @@ char *PROTO( re_pos_match, (char *, PTR, unsigned *) ) ;
 int   PROTO( binmode, (void)) ;
 
 
-int   PROTO( close, (int) ) ;
-int   PROTO( read, (int , PTR, unsigned) ) ;
-
 void PROTO ( parse, (void) ) ;
 int  PROTO ( yylex, (void) ) ;
 int  PROTO( yyparse, (void) ) ;
 void PROTO( yyerror, (char *) ) ;
 void PROTO( scan_cleanup, (void)) ;
 
-void PROTO( bozo, (char *) ) ;
+void PROTO( bozo, (char *) ) __attribute__ ((noreturn));
 void PROTO( errmsg , (int, char*, ...) ) ;
 void PROTO( compile_error, ( char *, ...) ) ;
 
