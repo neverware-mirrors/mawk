e7a28beafd3f35611247f8e2c2774e0bc1c8c920
Boyuan Yang <byang@debian.org>
2019-12-27 22:08:04 -0800

    New upstream version 1.3.4.20190203

--- patchlev.h
+++ patchlev.h
@@ -1,5 +1,19 @@
-/* mawk 1.3 */
-#define  PATCHLEVEL	3
-#define  PATCH_STRING	".3"
-#define  DATE_STRING    "Nov 1996"
-#define  MAWK_ID	"@(#)mawk 1.3.3"
+/*
+patchlev.h
+copyright 2009-2018,2019, Thomas E. Dickey
+copyright 1991-1996,2014, Michael D. Brennan
+
+This is a source file for mawk, an implementation of
+the AWK programming language.
+
+Mawk is distributed without warranty under the terms of
+the GNU General Public License, version 2, 1991.
+ */
+
+/*
+ * $MawkId: patchlev.h,v 1.90 2019/02/03 17:23:08 tom Exp $
+ */
+#define  PATCH_BASE	1
+#define  PATCH_LEVEL	3
+#define  PATCH_STRING	".4"
+#define  DATE_STRING    "20190203"
